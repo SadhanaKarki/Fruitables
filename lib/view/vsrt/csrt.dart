33ec0f1de10efc97d01956a5957a69d412bedf3b
SadhanaKarki <sadhanakarki141@gmail.com>
2025-03-24 23:32:20 +0545

    implemented cart

--- lib/view/vsrt/csrt.dart
+++ lib/view/vsrt/csrt.dart
@@ -14,7 +14,37 @@ class MyCart extends StatelessWidget {
           child: Container(
             child: Text("my cart"),
           ),
-        )
+        ),
+
+        Container(
+                  
+                  width: double.infinity,
+                  
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(255, 122, 204, 95),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+            
+                    children: [
+                      Text(
+                        "Seasonal Offer",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                      Text(
+                        "Flat 10% off on every item",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
       ],),
     );
   }
