b4ba84fbc483e0563670819ef297f1719f2085e1
SadhanaKarki <sadhanakarki141@gmail.com>
2025-04-12 22:53:45 +0545

    added new feature

---
+++ lib/view/my orders/myorders.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Myorders extends StatelessWidget {
+  const Myorders({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("my orders"),
+      ),
+    );
+  }
+}
